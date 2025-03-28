c14151294898b548c77c8198c2847e8cb94e515a
PierrickBlot <pierrickblot243@gmail.com>
2025-03-28 16:05:08 +0100

    created reality breaker

--- connected.js
+++ connected.js
@@ -15,11 +15,13 @@ function sleep(ms) {
 $('body').terminal({  
     help: function(command) {
         const commands = {
-            f: 'Available commands are: help, login, about, contact, clear, exit'
+            f: 'Available commands are: help, show_message, exec, about, clear, exit'
             +'\nUse the command "help <command>" to get more information about a specific command.',
             about: 'about - Provides information about the system.',
             clear: 'clear - Clears the terminal screen.',
-            exit: 'exit - Exits the terminal.'
+            exit: 'exit - Exits the terminal.',
+            show_message: 'show_message <message_number> - Displays a specific message. Use "show_message all" to see all messages.',
+            exec: 'exec <command> - Executes a specific command available'
         };
         if (commands[command]) {
             this.echo(commands[command]);
@@ -38,6 +40,63 @@ $('body').terminal({
         sleep(2000).then(()=>{
             location.href = 'index.html';});
     },
+    show_notification: function() {
+        this.echo('Notification 1: You have a meeting at 10:00 AM tomorrow');
+    },
+    show_message: function(message) {
+        if (message === 1) {
+            this.echo(
+                '\nObjet: Votre réunion a été reportée à demain 10 heures.\n'
+                +'From : Magistère Joshua Sanctus Aslan\n'
+                +'Date: Aujourd\'hui 10:05\n\n'
+                +'Bonjour Monarque,\n'
+                +'Notre réunion sur votre évaluation a été reportée à demain 10 heures.\n'
+                +'Merci de votre compréhension.\n'
+                +'Que le Monolithe vous guide,\n'
+                +'Magistère Joshua Sanctus Aslan\n');
+        } else if (message === 2) {
+            this.echo('\nObjet: Avec mes remerciements\n'
+                +'From : Hierophant Maxon Part\n'
+                +'Date: Aujourd\'hui 14:00\n\n'
+                +'Bonjour Monarque,\n'
+                +'Je vous remercie pour votre aide précieuse lors des dernières batailles Monarque.\n'
+                +'Je viens d\'apprendre votre échange avec le Magistère Joshua Sanctus Aslan.\n'
+                +'Je vous remercie de votre aide précieuse et qu\'importe votre décision, je vous soutiendrais.\n'
+                +'Que l\'équilibre guide vos pas,\n'
+                +'Hierophant Maxon Part\n');
+        } else if (message === 3) {
+            this.echo('Objet: votRE commAnde est vaLIdée\n'
+                +'From : TYphoon software\n'
+                +'Date: Aujourd\'hui 14:00\n\n'
+                +'Bonjour,\n'
+                +'votRE commAnde a été validée et vous avez du le recevoir. en esperant que votre Karma soit bon.\n'
+                +'mERci de votre confiance.\n'
+                +'typhoon software\n'
+                +'PS: Cela fait longtemps\n'
+            );
+        }else if (message === 'all') {
+            this.echo('Message 1: Votre réunion a été reportée à demain 10 heures.\n'
+            +'Message 2: Avec mes remerciements \n'
+            +'Message 3: votRE commAnde est vaLIdée\n');
+        } else {
+            this.echo('No message available for message number: ' + message);
+        }
+    },
+    exec: function(command){
+        const commands = {
+            reality_breaker: 'Executing Reality Breaker...',
+        };
+        if (commands[command]) {
+            this.echo(commands[command]);
+            if(command === 'reality_breaker') {
+                sleep(2000).then(()=>{
+                    location.href = 'reality_breaker.html';
+                });
+            }
+        } else {
+            this.echo('No information available for command: ' + command);
+        }
+    }
 }, 
 { 
     greetings: $top +'EquilibrumOS 2.5 - Personal assitant'
---
+++ reality_breaker.js
@@ -0,0 +1,162 @@
+const $top = `
+░        ░░░     ░░░.   ░░░░░░░▒▒▒           ▒▒▒▒░░░░░.  ░░░░░░░░    .     ░░░░
+  phoubia distro ░     ░░░░▒▒▒▒    ▄█ ▄██▄ █▄    ▒▒▒▒░░░ ░░░░▒░░░░░.   .░░░░░░░░
+░░░.  ░░░░░░░░░░░░   ░░░░▒▒    ▄▄███  ▀██▀  ███▄▄    ▒░░░░░░░░▒▒▒░░░░ ░░░░░░░░░
+░░░░ ░░░░▒░░░░░░░ ░░░░░░▒  ▄████████▄      ▄████████▄  ▒░░░░░░░░▒▒░░░░ ░░░░░░░.
+░▒░░░░░▒▒░░░░▒░░░░░░░░░░▒ ▀██████████████████████████▀ ▒░░░░░░░▒▒░░░░░░░░▒▒░░░░
+░░▒░░░▒▒░░░░░░▒░░░░  ░▒░░▒   ▀▀██████████████████▀▀   ▒░░░░░░▒▒▒▒▒░░░░░░▒░░░░░░░
+▒▒░ ░░░▒▒▒▒░░▒▒▒░░ █ ░░▒░░▒▒▒▒    ▀██████████▀    ▒▒▒▒░░░░░▒▒▒▒▒▒▒▒▒░░░▒▒▒▒▒░░░░
+▒▒ █ ░▒▒▒▒▒░      █ ▒░░▒▒▒▒▒▒▒▓▓▓▓  ████████  ▓▓▓▓▒▒▒░░░░▒▒▒▒▒▒▒▒▒▒▒▒░▒▒▒▒▒▒▒▒▒░
+▒ ██ ▒▒▒▒▒▒ ▄█████▄ ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓ ▀██████▀ ▓▒▒▒▒▒▒░▒▒▒▒         ▒▒▒▒        ▒▒
+▄███ ▒▒▒▒▒ ███▀ ████ ▒▒▒▒▒▒▒▒▒▒▒▒▒▓  ██████  ▓▒▒▒▒▒▒▒▒▒  ▄███████▄  ▒ ▄██████▀ ▒
+  ██ ▒▒▒▒ ▄██▀  █ ██          ▒▒▒▒▒▓ ██████ ▓▒▒▓▓▓▓▓▒▒ ▄██▀     ▀██▄  ██▀    ▒▒▒
+▓ ██▄     ██▀  █  ▀█ ▄██▄█████▄ ▓▓▓█ ▀████▀ █▓▓v3.6c ▓ ██ ▄█████▄ ██  ██▄     ▓▓
+▓ ██████▄ ██   █   ██ █████████▄ ▓▓█  ████  █▓▓▓▓▓▓▓▓▓ ██ ██   ██ ██  ▀██████▄ ▓
+▓ ██▀ ███ ██▄  █  ▄██ ██▀      ▀ ▓▓▓█  ██  █▓▓▓▓▓▓▓▓▓▓ ██ ▀█████▀ ██ ▓▓    ▀██ ▓
+▓ ██  ▀██ ▀██▄█  ▄██▀ ██  ▓▓▓      ▓▓█ ██ █▓           ▀██▄     ▄██▀       ▄██ ▓
+▓ ██ ▓ ██  ████▄▄███  ██ ▓▓  ▄█████ ▓█    █  ███████████ ▀███████▀ ██████████▀ ▓
+▓ ██ ▓ ██   ▀█████▀   ██ ▓ ▄██▀     ▓▓█  █▓▓             .                    ▓▓
+▓ ▀    ██ · █      .  ██   █▀  ∙     █    █  ·     .            ∙         ·    ▓
+▓   ∙  ▀█  █   ·      ██ . ▀     .    █  █      GREETS FROM TAU CETI ! !   .   ▓
+█ .     ▀    · .      ▀█    ∙    ·     ██  ∙                  ·    ∙           █
+█     ∙            ·   ▀                             ·   ∙  ·   .          ∙   █
+`;
+
+const $message1 =`
+%% OPENING PRIVATE CHANNEL %%
+%% DOWNLOADING ENCRYPTED PACKET ... %%
+%% [▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋] %%
+%% DECRYPTING PACKET ... %%
+%% [▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋] %%
+%% PACKET DECODED ! %%
+
+.../ CHANNEL OPENED\...
++ [User 1 joined]
+- [User 2 joined]
+
++ La Vistaria a perdu la guerre ... c'était ... inatendu...
+- Nos plans devront être changés. Nous ne pouvons pas laissé la situations dégénérés
++ Je suis surpris que Frygon ai été vaincu...
+- Cette unité de pèlerin n'est pas à sous estimé. S'ils n'avaient pas été là nous ...
++ Vos conjectures ne m'interesse guère. Nous devons rétablir notre stratégie sur des faits.
+- ... Bien. Ce sera fait.
+
+* [Xvhu#6 joined]
+
+* Oh#sodq#qh#fkdqjh#sdv#111
++ Oui, malheuresement ...
+* Pdlqwhqdqw/#uhwrxuqh}#d#yrv#srvwhv1#Mh#grlv#p*rffxsshu#g*xq#sureohph1#Hw#q*rxeolh}#sdv/#qrxv#vrpphv#ohv#yudl#vhuylwhxuv#gx#prqrolwkh
++ Très bien ... Je m'en occupe 
+
++ [User 1 leaved]
+
+- ... Il en sera ainsi
+
+- [User 2 leaved]
+
+* 12wudfnbibw1h{h#0i#2uhdolw|beuhdnbil{hu#.#yluwxrvrbvs| 
+%% CRITICAL ERROR SYSTEM %%
+%% REBOOTING OS %%
+%% ALERT CRITICAL FAILURE %%
+%% ACCESSING DATA ... %%
+. . .
+
+* I have found you ...
+
+%% CLOSING CONNECTION %%\n`
+
+const $message2 =`
+%% OPENING PRIVATE CHANNEL %%
+%% DOWNLOADING ENCRYPTED PACKET ... %%
+%% [▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋] %%
+%% DECRYPTING PACKET ... %%
+%% [▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋▋] %%
+%% PACKET DECODED ! %%
+
+.../ CHANNEL OPENED\...
++ [User 1 joined]
+- [Monarque joined]
+
++ Je savais que tu allais venir. Je t’attendais.
+
+- Je suis là pour te trouver...
+
++ Et moi, je suis là pour t’aider.
+
+- Je ne te fais pas confiance.
+
++ Je comprends. Moi aussi, j’étais dubitatif au début. Mais je suis là pour détruire ta vision du monde.
+
+- Tu es un traître.
+
++ Non. Je suis un ami. Je suis ici pour t’aider à voir la vérité.
+
+- Je ne veux pas de ton aide.
+
++ Tu n’as pas le choix. Je suis là pour te sauver. La police est déjà en route vers ta chambre. Tu dois fuir.
+
+- Je ne peux pas fuir. J’ai des responsabilités. Et des compagnons.
+
++ Tu crois vraiment que tes compagnons peuvent te protéger ? Tu n’as pas compris que tu es seul ?
+
+- Ils seront là pour moi. Ils m’aideront.
+
++ Tu es fou. Tu ne peux pas compter sur eux. Ils ne te comprendront jamais. Car toi seul connais la vérité.
+
+- ...
+
++ Si tu veux avoir une chance de t’en sortir, tu dois me faire confiance. Je suis là pour t’aider. RDV à ces coordonnées : 48.8566° N, 2.3522° E. Je t’attendrai là-bas.
+
+- Je ne peux pas te faire confiance.
+
++ Comme si tu avais le choix... Si tu ne le fais pas pour toi, fais-le pour eux.
+
+%% CLOSING CONNECTION %%\n`
+
+let $currentprompt = 'h0r_os> ';
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+$('body').terminal({  
+
+    help: function(command) {
+        const commands = {
+            f: 'Available commands are: help, login, about, contact, clear, exit'
+            +'\nUse the command "help <command>" to get more information about a specific command.',
+            clear: 'clear - Clears the terminal screen.',
+            exit: 'exit - Exits the terminal.'
+        };
+        if (commands[command]) {
+            this.echo(commands[command]);
+        } else {
+            this.echo('No information available for command: ' + command);
+        }
+    },
+    about: function() {
+        this.echo('h0r_os v3.6c - 30% chance of update available\n');
+    },
+    show_reality:function(message){
+        const messages = {
+            1: $message1,
+            2: $message2,
+            'all': '1. 15:03:52 \n2. 17:16:34',
+        };
+        if (messages[message]) {
+            this.echo(messages[message]);
+        } else {
+            this.echo('No information available for message: ' + message);
+        }
+    },
+    clear: function() {
+        this.clear();
+    },
+    exit: function() {
+        this.echo('Exiting h0r_os - we vill be watching...');
+    },
+}, 
+{ 
+    greetings: $top +'h0r_os - Welcome ... Vindicators -'
+    +' use the command "help f" to know more\n',
+    prompt: $currentprompt    
+}); 
